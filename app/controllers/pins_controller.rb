b1bbf345051d77f5b0f881bee81f2f4a8dcf49f6
Garret Tullio <garret12@uw.edu>
2020-05-18 16:08:02 -0700

    Associated pins with users and authenticated users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,25 +1,26 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   respond_to :html
 
   # GET /pins
   # GET /pins.json
   def index
     @pins = Pin.all
-    respond_with(@pins)
   end
 
   # GET /pins/1
   # GET /pins/1.json
   def show
-    respond_with(@pin)
   end
 
   # GET /pins/new
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
   end
+  
 
   # GET /pins/1/edit
   def edit
@@ -28,23 +29,29 @@ class PinsController < ApplicationController
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created'
+    else
+      render action: 'new'
+    end
   end
 
   # PATCH/PUT /pins/1
   # PATCH/PUT /pins/1.json
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end  
   end
 
   # DELETE /pins/1
   # DELETE /pins/1.json
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to pins_url
   end
 
   private
@@ -57,4 +64,11 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description)
     end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" 
+        if @pin.nil?
+        end
+    end 
 end

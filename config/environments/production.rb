8c2125829c9d9f8f51f9909ce22d58ba5ca562c0
Garret Tullio <garret12@uw.edu>
2020-05-19 14:52:18 -0700

    Manually entered the bucket option into production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,8 +115,8 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     s3_region: 'us-west-2',
+    :bucket => 'pinterest-bucket',
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }

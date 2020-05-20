53bf25a88396d8102f3b95310475bd8b72a7d22a
Garret Tullio <garret12@uw.edu>
2020-05-19 22:57:59 -0700

    Removed paperclip initializer file

--- config/initializers/paperclip.rb
+++
@@ -1,3 +0,0 @@
-Paperclip::Attachment.default_options[:url] = ':pinterest-bucket.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-
